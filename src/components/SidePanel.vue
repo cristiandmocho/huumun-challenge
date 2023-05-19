72fd4739f88ec23610669c99fc118f86e5d6e2cd
Cristian D. Mocho <cristiandmocho@gmail.com>
2023-05-18 21:06:34 -0300

    Creating components (#2)
    Co-authored-by: Cristian Mocho <cristian@computech.ie>

---
+++ src/components/SidePanel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="`side-panel ${open ? 'open' : ''}`">
+    <h3>{{ film != null ? film.url : '' }}</h3>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  open: Boolean,
+  film: { title: '', director: '', producer: '', release_date: '', opening_crawl: '', url: '' },
+});
+</script>
+
+<style lang="scss" scoped>
+.side-panel {
+  display: grid;
+  position: absolute;
+  height: 100%;
+  width: 600px;
+  top: 0;
+  left: -610px;
+  z-index: 2;
+  background-color: #ddd;
+  box-shadow: rgba(0, 0, 0, 0.5) 0 0 12px 3px;
+  padding: 2em;
+  transition: all 0.5s ease-out;
+
+  &.open {
+    left: 0;
+  }
+}
+</style>
